76fc82bcb9d3a1250bc5bf8c602cf10a21945529
person4268 <person4268@person4269.cf>
2020-01-16 18:12:52 -0500

    did thing

---
+++ app/app.ino
@@ -0,0 +1,16 @@
+#include "wammy.h"
+#include "strum.h"
+#include "HardwareSerial.h"
+
+void setup()
+{
+    strum_init();
+    wammy_init();
+    Serial.begin(9600);
+}
+
+void loop()
+{
+	//strum_read();
+    wammy_read();
+}
